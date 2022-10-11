68e875c0671fe148e52613b294dda6bc6cc166ed
Hans Larsen <hans@larsen.online>
2022-10-11 16:03:11 -0700

    Specification for tokens and create/mint/burn
    This specification goes into details of what a token should contain
    as information, update the ledger information regarding it (and
    returns token informations instead of just names), and add a new
    attribute for creating / minting / burning tokens.
    
    It also includes account roles so a token owner can be an account
    and contains roles for others.

--- scripts/make_cddl.bash
+++ scripts/make_cddl.bash
@@ -5,12 +5,12 @@ find_cddl() {
 
     # List all in directory order (breadth-first), and remove the files
     # that aren't part of the repository from the list.
-    for i in 0 1 2 3 4 5 6 7 8; do
-        find "$1" -mindepth "$i" -maxdepth "$i" -iname \*.cddl -print0
-    done
+    find "$1" -maxdepth 0 -iname \*.cddl | sort -n
+    find "*/$1" -maxdepth 0 -iname \*.cddl | sort -n || true
+
 }
 
 {
-    find_cddl "$(dirname "$(dirname "$0")")"/spec | xargs --null cat
-    find_cddl "$(dirname "$(dirname "$0")")"/attributes | xargs --null cat
+    find_cddl "$(dirname "$(dirname "$0")")"/spec #| xargs -0 echo
+    find_cddl "$(dirname "$(dirname "$0")")"/attributes #| xargs -0 echo
 } > "$1"
