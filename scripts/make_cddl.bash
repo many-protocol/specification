f1652d08be015f498576327ce6ab19319efe38fd
Hans Larsen <hans@larsen.online>
2022-10-12 12:25:44 -0700

    Fix CI by adding an order to the evaluation of rules
    Before it was alphabetical, now we do breadth-first to allow for types
    defined in directories above to be used. We also now use the gitignore
    rules instead of just singling out target.

---
+++ scripts/make_cddl.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+find_cddl() {
+    local i
+
+    # List all in directory order (breadth-first), and remove the files
+    # that aren't part of the repository from the list.
+    for i in 0 1 2 3 4 5 6 7 8; do
+        find "$1" -mindepth $i -maxdepth $i -iname \*.cddl \
+            | xargs git ls-files --exclude-standard
+    done
+}
+
+find_cddl "$(dirname "$(dirname "$0")")" | xargs cat > "$1"
