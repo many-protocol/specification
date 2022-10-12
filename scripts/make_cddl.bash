42c7c7c6e15139870541cebd1f601f2b5ae244d7
Hans Larsen <hans@larsen.online>
2022-10-12 14:21:32 -0700

    .

--- scripts/make_cddl.bash
+++ scripts/make_cddl.bash
@@ -8,14 +8,14 @@ find_cddl() {
     # List all in directory order (breadth-first), and remove the files
     # that aren't part of the repository from the list.
     for i in 0 1 2 3 4 5 6 7 8; do
-        find "$1" -mindepth "$i" -maxdepth "$i" -iname \*.cddl -print0 \
-            | xargs -0 git ls-files --exclude-standard
+        find "$1" -mindepth "$i" -maxdepth "$i" -iname \*.cddl -print0
     done
 }
 
-find_cddl "$(dirname "$(dirname "$0")")"
-echo '-----------'
-find_cddl "$(dirname "$(dirname "$0")")" | xargs cat > "$1"
+{
+    find_cddl "$(dirname "$(dirname "$0")")"/spec | xargs cat
+    find_cddl "$(dirname "$(dirname "$0")")"/attributes | xargs cat
+} > "$1"
 
 echo "$1":
 cat "$1"
