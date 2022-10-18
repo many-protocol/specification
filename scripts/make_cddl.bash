0a3962021a3dc85ad8128d8113e5ae617475dcdc
Hans Larsen <hans@larsen.online>
2022-10-18 16:41:05 -0700

    Fix make_cddl.bash once and for all.
    ONCE. AND FOR ALL.

--- scripts/make_cddl.bash
+++ scripts/make_cddl.bash
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
 find_cddl() {
-    local i
+    local i filename
 
     # List all in directory order (breadth-first), and remove the files
     # that aren't part of the repository from the list.
-    find "$1" -maxdepth 0 -iname \*.cddl | sort -n
-    find "*/$1" -maxdepth 0 -iname \*.cddl | sort -n || true
-
+    # Sort by basename.
+    for i in 0 1 2 3 4 5 6 7 8; do
+        find "$1" -mindepth "$i" -maxdepth "$i" -iname \*.cddl | while read -r filename
+        do
+            echo "$(basename "$filename"):$filename"
+        done | sort -n | awk -F: '{ print $2 }'
+    done
 }
 
 {
-    find_cddl "$(dirname "$(dirname "$0")")"/spec #| xargs -0 echo
-    find_cddl "$(dirname "$(dirname "$0")")"/attributes #| xargs -0 echo
-} > "$1"
+    find_cddl "$(dirname "$(dirname "$0")")"/spec
+    find_cddl "$(dirname "$(dirname "$0")")"/attributes/network
+    find_cddl "$(dirname "$(dirname "$0")")"/attributes/request
+    find_cddl "$(dirname "$(dirname "$0")")"/attributes/response
+} | xargs cat > "$1"
