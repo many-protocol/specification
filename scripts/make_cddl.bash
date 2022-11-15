dc35778d381ea331b0f05dcae0a3daa8d490867c
Hans Larsen <hans@larsen.online>
2022-11-15 09:24:15 -0800

    Fix CI

--- scripts/make_cddl.bash
+++ scripts/make_cddl.bash
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
 find_cddl() {
-    local i
+    local i filename
 
     # List all in directory order (breadth-first), and remove the files
     # that aren't part of the repository from the list.
+    # Sort by basename.
     for i in 0 1 2 3 4 5 6 7 8; do
-        find "$1" -mindepth "$i" -maxdepth "$i" -iname \*.cddl -print0
+        find "$1" -mindepth "$i" -maxdepth "$i" -iname \*.cddl | while read -r filename
+        do
+            echo "$(basename "$filename"):$filename"
+        done | sort -n | awk -F: '{ print $2 }'
     done
 }
 
 {
-    find_cddl "$(dirname "$(dirname "$0")")"/spec | xargs --null cat
-    find_cddl "$(dirname "$(dirname "$0")")"/attributes | xargs --null cat
-} > "$1"
+    find_cddl "$(dirname "$(dirname "$0")")"/spec
+    find_cddl "$(dirname "$(dirname "$0")")"/attributes/network
+    find_cddl "$(dirname "$(dirname "$0")")"/attributes/request
+    find_cddl "$(dirname "$(dirname "$0")")"/attributes/response
+} | xargs cat > "$1"
